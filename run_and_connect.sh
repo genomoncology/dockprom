0534883e4760fb9ad250592ef3ca81f4444254b8
Jake Peacock <jake@genomoncology.com>
2019-06-20 16:32:09 -0400

    Update run and connect script. Add django metrics to prometheus

--- run_and_connect.sh
+++ run_and_connect.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-ADMIN_USER=admin ADMIN_PASSWORD=admin docker-compose up -d
+if [ $# -gt 0 ] && [ $1 == --dev ]
+then
+    echo "In Dev Mode"
+    ADMIN_USER=admin ADMIN_PASSWORD=admin docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+else
+   ADMIN_USER=admin ADMIN_PASSWORD=admin docker-compose up -d
+fi
 
 docker network connect knowledge_default caddy
+docker network connect knowledge_default prometheus
 
 export CADDY_IP=$(docker inspect knowledge_default > network.txt && grep -A3 "caddy" network.txt | tail -n 1 | cut -d'"' -f 4 | cut -d'/' -f 1) && echo "ssh -L 3000:${CADDY_IP}:3000 -N ${HOSTNAME}.genomoncology.io" && rm -f network.txt
